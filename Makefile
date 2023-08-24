fc200b2b9125c73d48b8a0f63d10e7ccec29fb31
Dilip Kumar Sharma <diliplakshya@gmail.com>
2023-08-24 17:23:55 +0530

    Add makefile

--- Makefile
+++ Makefile
@@ -3,16 +3,17 @@ all: stop up
 stop:
 	docker compose stop
 
+down:
+	docker compose down
+
 clean:
 	docker compose stop
+	docker compose down
 	docker container prune -f
 	docker image prune -f
 
 up:
 	docker compose up -d --build --remove-orphans
 
-test:
-	docker exec backend pytest -v
-
-cov:
-	docker exec backend pytest --cov
+ps:
+	docker compose ps -a
--- kafka_app/main.py
+++ kafka_app/main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
-
+from kafka import KafkaProducer
+import json
 
 app = FastAPI()
 
+producer = KafkaProducer(
+    bootstrap_servers=["kafka:29092", "kafka:39092", "kafka:49092"],
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+)
+
 
 @app.route("/home")
-def home():
+def home(name: str):
+    producer.send("mytopic", f"message from {name}")
     return "Namaste"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host='0.0.0.0', port=5001, reload=True)
+
+    uvicorn.run("main:app", host="0.0.0.0", port=5001, reload=True)
