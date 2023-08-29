9e81903c379165e424310891b941bcefbfe15d42
Dilip Kumar Sharma <diliplakshya@gmail.com>
2023-08-29 14:24:00 +0530

    produce msg from fastapi app

--- kafka_app/main.py
+++ kafka_app/main.py
@@ -6,17 +6,21 @@ app = FastAPI()
 
 producer = None
 
-@app.on_event("startup")
-async def startup_event():
+# @app.on_event("startup")
+# async def startup_event():
+#     producer = KafkaProducer(
+#         bootstrap_servers=["kafka-1:9093", "kafka-1:9094"],
+#         value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+#     )
+
+@app.post("/order/<count>")
+def home(count: int):
     producer = KafkaProducer(
         bootstrap_servers=["kafka-1:9093", "kafka-1:9094"],
         value_serializer=lambda v: json.dumps(v).encode("utf-8"),
     )
-@app.post("/order/<count>")
-def home(count: int):
     producer.send("mytopic", f"message from {count}")
     return count
-
 
 if __name__ == "__main__":
     import uvicorn
